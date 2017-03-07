367b62d494ebf650fad17ddcd281e4194c2dfebe
黄耀 <906521597@qq.com>
2017-03-07 20:30:55 +0800

    修改跨域条件

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require( 'express' ),
 			bodyParser = require('body-parser'),
-			cors = require( 'cors' ),
 			mongoose = require( 'mongoose' ),
 			compression = require( 'compression' )
 const rootRouter = require( './src/router' )
@@ -9,8 +8,10 @@ const rootRouter = require( './src/router' )
 mongoose.connect( 'mongodb://localhost/runnong' )
 
 const app = express()
-// 设置跨域
-app.use( cors() )
+
+// 设置跨域 - 开发的时候需要打开
+//app.use( require('cors')() )
+
 // 解析请求数据
 app.use( bodyParser.json() )  //解析 application/json 的请求头
 app.use( bodyParser.urlencoded({ extended: false }) )  // 解析 application/x-www-form-urlencoded 的请求头
